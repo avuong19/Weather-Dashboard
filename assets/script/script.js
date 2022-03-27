c54032b51ae571a7f9f37e55e330a6f3ad98995f
Anh Vuong <vta19797@gmail.com>
2022-03-27 01:16:23 -0500

    build html template and fetch geocoding api

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,29 +4,48 @@ var zipCodeEl=$('#zipCode').val();
 
 
 $('#searchBtn').on("click",function(){
-    
+    var zipCodeEl=$('#zipCode').val();
     // format the github api url
-  //var apiUrl = "http://api.openweathermap.org/geo/1.0/zip?zip="+zipCodeEl+"&appid="+ apiID;
+  var geoCodingUrl = "http://api.openweathermap.org/geo/1.0/zip?zip="+zipCodeEl+"&appid="+ apiID;
 
   // make a get request to url
-  //fetch(apiUrl)
-   // .then(function(response) {
+  fetch(geoCodingUrl)
+    .then(function(response) {
       // request was successful
-     // if (response.ok) {
-       // console.log(response);
-       // response.json().then(function(data) {
-        //  console.log(data);
-          
-          
-       // });
-     // } else {
-       // alert("Error: Please enter a valid zip code");
-     // }
-   // })
-   // .catch(function(error) {
-   //   alert("Unable to connect to the API");
-   // });
+      if (response.ok) {
+       response.json().then(function(data) {
+          getWeather(data);
+         });
+      } else {
+        alert("Error: Please enter a valid zip code");
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect to the API");
+    });
 
-  console.log(zipCodeEl);
+   
 
 });
+var getWeather =function(data){
+    var lat=data.lat;
+    var lon=data.lon;
+    
+    var weatherApi="https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude={part}&appid="+apiID;
+    
+
+    fetch(weatherApi)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+       response.json().then(function(data) {
+          console.log(data);
+         });
+      } else {
+        alert("Error: something is wrong");
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect to the API");
+    });
+};
