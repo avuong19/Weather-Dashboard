0ec61cfd9f99a58aaa1cd794350187646cbcc680
Anh Vuong <vta19797@gmail.com>
2022-03-27 22:09:05 -0500

    show weather forecast for the next 5 days

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,6 +6,7 @@ var zipCodeEl=$('#zipCode').val();
 
 
 var searchLocation=function(){
+    
     var zipCodeEl=$('#zipCode').val();
     // format the github api url
   var geoCodingUrl = "http://api.openweathermap.org/geo/1.0/zip?zip="+zipCodeEl+"&appid="+ apiID;
@@ -56,7 +57,11 @@ var getWeather =function(data){
     var getWeatherInfo=function(info){
         var timeStamp=info.current.dt;
         var timeStamformated=moment.unix(timeStamp).format('L');
-        
+        var icon=info.current.weather[0].icon;
+        var iconApi="http://openweathermap.org/img/wn/"+icon+"@2x.png"
+        document.getElementById("icon").setAttribute("src",iconApi);
+        console.log(iconApi);
+
 
         var getCityName=function(){
             $('#cityHeader').text(cityName+ " ("+timeStamformated+')');
@@ -93,7 +98,62 @@ var getWeather =function(data){
         }
         getCurrentUVIndex();
         
-        
+        for(i=1;i<=5;i++){
+            var futureTimeStamp=info.daily[i].dt;
+            var futuretimeStamformated=moment.unix(futureTimeStamp).format('L');
+            var futureIcon=info.daily[i].weather[0].icon;
+            var futureiconApi="http://openweathermap.org/img/wn/"+futureIcon+"@2x.png"
+            
+
+            var fiveDayForeCastEl=document.querySelector('#fiveDayForecast');
+            //create the card for information 
+            var card=document.createElement("div");
+            card.classList="card col-lg-auto col-sm-auto col-md-auto m-3";
+            fiveDayForeCastEl.appendChild(card);
+
+            var cardHeader=document.createElement("div");
+            cardHeader.classList="card-header";
+            card.appendChild(cardHeader);
+            cardHeader.innerHTML=futuretimeStamformated;
+
+            var cardBody=document.createElement("div");
+            cardBody.classList="card-body";
+            card.appendChild(cardBody);
+
+            var cardTitle=document.createElement("img");
+            cardTitle.classList="card-title";
+            cardTitle.setAttribute("id","cardTitle");
+            cardBody.appendChild(cardTitle);
+            cardTitle.setAttribute("src",futureiconApi);
+            
+
+            var cardText=document.createElement("p");
+            cardText.classList="card-text";
+            cardBody.appendChild(cardText);
+
+            var futureTemp=document.createElement("p");
+            futureTemp.setAttribute("id","futureTemp");
+            cardText.appendChild(futureTemp);
+            futureTemp.innerHTML="Temp: "+ info.daily[i].temp.day+ " F";
+
+            var futureWind=document.createElement("p");
+            futureWind.setAttribute("id","futureWind");
+            cardText.appendChild(futureWind);
+            futureWind.innerHTML="Wind: "+ info.daily[i].wind_speed+ " MPH";
+
+
+            var futureHumidity=document.createElement("p");
+            futureHumidity.setAttribute("id","futureHumidity");
+            cardText.appendChild(futureHumidity);
+            futureHumidity.innerHTML="Humidity: "+ info.daily[i].humidity+ " %";
+            
+
+            var futureUVI=document.createElement("p");
+            futureUVI.setAttribute("id","futureUVI");
+            cardText.appendChild(futureUVI);
+            futureUVI.innerHTML="UV Index: "+ info.daily[i].uvi;
+            
+        }
 
 
     }
